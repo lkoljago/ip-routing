96f649a2ec923af79b2cc229a9bfab9c3e3602ae
lkoljago <l.koljago@gmail.com>
2020-06-05 15:18:39 +0300

    add cors

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use(expressip().getIpInfoMiddleware);
 app.set("PORT", PORT);
 
 app.get('/', function (req, res) {
-    console.log(res);
+    console.log(req);
     
     res.json({ mess: req.ipInfo });
 });
