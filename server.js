407ed2a55622ee8e23112798477c06c555412c4d
lkoljago <l.koljago@gmail.com>
2020-06-05 15:11:30 +0300

    add cors

--- server.js
+++ server.js
@@ -4,16 +4,19 @@ const expressip = require('express-ip');
 const PORT = process.env.PORT || 7000;
 const path = require('path');
 
+var cors = require('cors')
+app.use(cors())
+
 app.use(expressip().getIpInfoMiddleware);
 
 
 app.set("PORT", PORT);
 
 app.get('/', function (req, res) {
-    res.send(req.ipInfo);
+    res.json({ mess: req.ipInfo });
 });
 
 app.listen(app.get('PORT'), function () {
     console.log('Express started on http://localhost:' +
-        app.get('PORT') + '; press Ctrl-C to terminate.');
+        app.get('PORT') + '; press Ctrl-C to terminate. CORS enabled');
 });
